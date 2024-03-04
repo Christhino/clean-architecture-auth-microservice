9a240674543a225e624ca0183abdfd46fa1c521d
christhino <mintsaniaina.christhino@gmail.com>
2024-03-04 21:06:11 +0300

    fix(env): update configuration

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,13 +1,13 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || '',
-  port: process.env.PORT || '',
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://mintsaniainachristhino:bggrKtKDBWHFyXsN@auth.ehi6d6x.mongodb.net/?retryWrites=true&w=majority&appName=Auth',
+  port: process.env.PORT || '5050',
   jwtSecret:
     process.env.JWT_SECRET ||
     'e9cfc88d48b675ebdb4bdbf7384e5641d78d79f7192bb9823a1fece99b4078ab',
-  smtpMail: process.env.SMTP_MAIL || '',
-  smtpHost: process.env.SMTP_HOST || '',
+  smtpMail: process.env.SMTP_MAIL || 'mintsaniaina.christhino@gmail.com',
+  smtpHost: process.env.SMTP_HOST || 'smtp-relay.brevo.com',
   smtpPort: process.env.SMTP_PORT || 587,
-  smtpUser: process.env.SMTP_USER || '',
-  smtpPass: process.env.SMTP_PASS || '',
+  smtpUser: process.env.SMTP_USER || 'mintsaniaina.christhino@gmail.com',
+  smtpPass: process.env.SMTP_PASS || 'dGLRmFCO3kvbU24I',
   salt: 12,
 };
